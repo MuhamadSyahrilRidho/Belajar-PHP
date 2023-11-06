89de4e3725c196565e8de1bbf1e21b4f88eb2769
MuhamadSyahrilRidho <135937034+MuhamadSyahrilRidho@users.noreply.github.com>
2023-11-06 14:44:49 +0700

    awalan

--- Config.php
+++ Config.php
@@ -1,1 +1,11 @@
-$host 
+<?php
+$host = 'localhost';
+$db = 'if22';
+$user = 'if22';
+$pwd = '12345';
+
+$conn = mysqli_connect($host, $user, $pwd, $db); // true | false
+
+if (!$conn) {
+  die('Gagal terhubung ke database'. mysqli_connect_error());
+}
